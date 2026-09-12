5dd2e0d65af3e8a44fce7eea97d11cf56d99c86d
Glorou <leaguehatesme@gmail.com>
2026-09-12 09:43:35 -0400

    structs

--- dllmain.cpp
+++ dllmain.cpp
@@ -4,51 +4,77 @@
 
 
 
-static void* Address = nullptr;  //one we monitor
-static unsigned int sizeOf = 0;  //Size of the type
-static void* VectoredExceptionHandlerPtr = nullptr;
-static void* _callback = nullptr;
-static void* _referencedAddress = nullptr;//actual address that got referenced
-const int framesToCapture = 3;
-static PVOID FramePointers[framesToCapture] = {0};
+Address_Info *_addresses = nullptr;  //one we monitor
+int _sizeofAddresses = 0;  //Size of the type
+void* VectoredExceptionHandlerPtr = nullptr;
+Access_Info* _accessInfo = nullptr;
+int _framesToCapture = 3;
+int _sizeOfInfo = 20;
 
 
 
-static bool AttachVEH() {
+bool AttachVEH() {
 	VectoredExceptionHandlerPtr = AddVectoredExceptionHandler(1, VectoredExceptionHandler);
 	if (VectoredExceptionHandlerPtr == nullptr)
 		return false;
 	return true;
 }
-static bool DetachVEH() {
+bool DetachVEH() {
 	int out = RemoveVectoredExceptionHandler(VectoredExceptionHandler);
+
 	if (out == 0)
 		return true;
 	return false;
 
 }
-static bool MonitorAddress(void* address, unsigned int sizeOfObject) {
-	if (Address != nullptr)
-		RemoveGuardPage();
-	Address = address;
-	sizeOf = sizeOfObject;	
-	return GuardPage();
+bool RefreshAddresses() {
+	if (_addresses == nullptr)
+		return false;
+
+	bool retval = true;
+	for (int i = 0; i < _sizeofAddresses; i++) {
+		if (_addresses[i].addressToWatch != 0 && !_addresses[i].watched) {
+			_addresses[i].watched = true;
+			retval &= GuardPage(_addresses[i].addressToWatch);
+		}
+	}
+	
+
+	return retval;
 }
-static void SetCallback(void* callback) {
-	_callback = callback;
+bool RemoveAddress(unsigned __int64 address) {
+	return RemoveGuardPage(address);
 }
-static bool GuardPage() {
+
+void SetupParams(Access_Info* array, int sizeOfInfo, int framesToCap, Address_Info* addresses, int sizeOfAddresses) {
+	_accessInfo = array;
+	_sizeOfInfo = sizeOfInfo;
+	_framesToCapture = framesToCap;
+	_addresses = addresses;
+	_sizeofAddresses = sizeOfAddresses;
+
+}
+
+bool GuardPage(unsigned __int64 Address) {
 	if (VectoredExceptionHandlerPtr == nullptr)
 		return false;
 	PMEMORY_BASIC_INFORMATION info = new MEMORY_BASIC_INFORMATION;
 
-	int e = VirtualQueryEx(GetCurrentProcess(), Address, info, sizeof(MEMORY_BASIC_INFORMATION));
+	int e = VirtualQueryEx(GetCurrentProcess(), (void*)Address, info, sizeof(MEMORY_BASIC_INFORMATION));
 
 	if (e == 0) {
 		HRESULT err = HRESULT_FROM_WIN32(GetLastError());
 		delete info;
 		return false;
 	}
+
+	if (info->Protect & PAGE_GUARD) {
+		delete info;
+		return true; //we already guarded this section of memory
+
+	}
+
+
 	info->Protect |= PAGE_GUARD;
 	DWORD tmp = 0;
 	e = VirtualProtectEx(GetCurrentProcess(), info->BaseAddress, 1, info->Protect, &tmp);
@@ -61,11 +87,11 @@ static bool GuardPage() {
 	return true;
 }
 
-static bool RemoveGuardPage() {
+bool RemoveGuardPage(unsigned __int64 Address) {
 
 	PMEMORY_BASIC_INFORMATION info = new MEMORY_BASIC_INFORMATION;
 
-	int e = VirtualQueryEx(GetCurrentProcess(), Address, info, sizeof(MEMORY_BASIC_INFORMATION));
+	int e = VirtualQueryEx(GetCurrentProcess(), (void*)Address, info, sizeof(MEMORY_BASIC_INFORMATION));
 
 	if (e == 0) {
 		HRESULT err = HRESULT_FROM_WIN32(GetLastError());
@@ -74,7 +100,7 @@ static bool RemoveGuardPage() {
 	}
 
 
-	if (info->Protect && PAGE_GUARD) {
+	if (info->Protect & PAGE_GUARD) {
 		info->Protect &= ~PAGE_GUARD;
 		DWORD tmp = 0;
 		e = VirtualProtectEx(GetCurrentProcess(), info->BaseAddress, 1, info->Protect, &tmp);
@@ -89,14 +115,24 @@ static bool RemoveGuardPage() {
 	return true;
 }
 
-static long VectoredExceptionHandler(_EXCEPTION_POINTERS* ExceptionInfo) {
+long VectoredExceptionHandler(_EXCEPTION_POINTERS* ExceptionInfo) {
 
 
 	if (HRESULT_FROM_WIN32(ExceptionInfo->ExceptionRecord->ExceptionCode) == STATUS_GUARD_PAGE_VIOLATION) {
-		if (ExceptionInfo->ExceptionRecord->ExceptionInformation[2] >= (unsigned long)Address && ExceptionInfo->ExceptionRecord->ExceptionInformation[2] <= (unsigned long)Address + sizeOf) {
-			_referencedAddress = (void*)ExceptionInfo->ExceptionRecord->ExceptionInformation[2];
-			RtlCaptureStackBackTrace(0, 3, FramePointers, nullptr);
+		for (int i = 0; i < _sizeofAddresses; i++) {
+			if (_addresses[i].addressToWatch != 0 && ExceptionInfo->ExceptionRecord->ExceptionInformation[1] >= (unsigned long)_addresses[i].addressToWatch && ExceptionInfo->ExceptionRecord->ExceptionInformation[1] <= (unsigned long)_addresses[i].addressToWatch + _addresses[i].sizeOfType) {
+				_addresses[i].watched = false; //page needs to be reset
+				for (int i = 0; i < _sizeOfInfo; i++) {
+					if (!_accessInfo[i].inUse) {
+						_accessInfo[i].addressAccessed = ExceptionInfo->ExceptionRecord->ExceptionInformation[1];
+						RtlCaptureStackBackTrace(0, _framesToCapture, (void**)(_accessInfo[i].frameTrace), nullptr);
+						_accessInfo[i].inUse = true;
+						break;
+					}
+				}
+			}
 		}
+
 		ExceptionInfo->ContextRecord->ContextFlags |= CONTEXT_CONTROL;
 		ExceptionInfo->ContextRecord->EFlags |= 0x0100;
 		return EXCEPTION_CONTINUE_EXECUTION;	//Swallow all the page guards cause idgaf
@@ -104,10 +140,12 @@ static long VectoredExceptionHandler(_EXCEPTION_POINTERS* ExceptionInfo) {
 	else if (HRESULT_FROM_WIN32(ExceptionInfo->ExceptionRecord->ExceptionCode) == STATUS_SINGLE_STEP) {
 		ExceptionInfo->ContextRecord->ContextFlags &= ~CONTEXT_CONTROL;
 		ExceptionInfo->ContextRecord->EFlags &= ~0x0100;
-		GuardPage();
-		
-		if (_callback != nullptr)
-			((void(*)(void*, PVOID*))_callback)(_referencedAddress, FramePointers);
+		for (int i = 0; i < _sizeofAddresses; i++) {
+			if (_addresses[i].addressToWatch != 0 && !_addresses[i].watched) {
+				GuardPage(_addresses[i].addressToWatch);
+				_addresses[i].watched = true;
+			}
+		}
 		return EXCEPTION_CONTINUE_EXECUTION;
 	}
 	return EXCEPTION_CONTINUE_SEARCH;
--- ipfd.h
+++ ipfd.h
@@ -1,9 +1,23 @@
 #pragma once
 
+struct Access_Info {
+	bool inUse;
+	unsigned __int64 addressAccessed;
+	unsigned __int64 frameTrace[];
+};
+struct Address_Info {
+	unsigned __int64 addressToWatch;
+	unsigned __int64 sizeOfType;
+	bool watched;
+};
+
 extern "C" __declspec(dllexport) bool AttachVEH();
 extern "C" __declspec(dllexport) bool DetachVEH();
-extern "C" __declspec(dllexport) bool MonitorAddress(void* address, unsigned int sizeOfObject);
-extern "C" __declspec(dllexport) void SetCallback(void* callback);
-static bool GuardPage();
-static bool RemoveGuardPage();
-static long VectoredExceptionHandler(_EXCEPTION_POINTERS* ExceptionInfo);
+extern "C" __declspec(dllexport) bool RefreshAddresses();
+extern "C" __declspec(dllexport) bool RemoveAddress(unsigned __int64 address);
+extern "C" __declspec(dllexport) void SetupParams(Access_Info* array, int sizeOfInfo, int framesToCap, Address_Info* addresses, int sizeOfAddresses);
+
+bool GuardPage(unsigned __int64 Address);
+bool RemoveGuardPage(unsigned __int64 Address);
+long VectoredExceptionHandler(_EXCEPTION_POINTERS* ExceptionInfo);
+
