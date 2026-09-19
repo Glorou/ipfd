40f229a19183e80fb85740b288a9fcd395c3c43c
Glorou <leaguehatesme@gmail.com>
2026-09-19 16:54:18 -0400

    holy fuck it works

--- dllmain.cpp
+++ dllmain.cpp
@@ -1,16 +1,18 @@
 // dllmain.cpp : Defines the entry point for the DLL application.
 #include "pch.h"
 #include "ipfd.h"
+#include <cstdint>
 
 
 
 Address_Info *_addresses = nullptr;  //one we monitor
-int _sizeofAddresses = 0;  //Size of the type
+int _sizeofAddresses = 0; 
 void* VectoredExceptionHandlerPtr = nullptr;
 Access_Info* _accessInfo = nullptr;
 int _framesToCapture = 3;
 int _sizeOfInfo = 20;
 unsigned long long _addressHit = 0;
+bool _unhook = false;
 PMEMORY_BASIC_INFORMATION info = new MEMORY_BASIC_INFORMATION;
 
 
@@ -53,6 +55,7 @@ bool RefreshAddresses() {
 	return retval;
 }
 bool RemoveAddress(unsigned __int64 address) {
+	_unhook = true;
 	return RemoveGuardPage(address);
 }
 
@@ -124,17 +127,46 @@ bool RemoveGuardPage(unsigned __int64 Address) {
 }
 
 static long CALLBACK VectoredExceptionHandler(PEXCEPTION_POINTERS ExceptionInfo) {
+	if (_unhook) {
+		constexpr uint64_t DR0_MASK = 0x000F0003ull;
+		constexpr uint64_t DR1_MASK = 0x00F0000Cull;
+		constexpr uint64_t DR2_MASK = 0x0F000030ull;
+		constexpr uint64_t DR3_MASK = 0xF00000C0ull;
+		uint64_t newBits = DR0_MASK | DR1_MASK | DR2_MASK | DR3_MASK;
 
+		ExceptionInfo->ContextRecord->Dr0 = ExceptionInfo->ContextRecord->Dr1 = ExceptionInfo->ContextRecord->Dr2 = ExceptionInfo->ContextRecord->Dr3 =  0;
+
+		ExceptionInfo->ContextRecord->Dr7 = ~newBits;
+		return EXCEPTION_CONTINUE_EXECUTION;
+	}
+	if (ExceptionInfo->ExceptionRecord->ExceptionCode == STATUS_BREAKPOINT || ExceptionInfo->ExceptionRecord->ExceptionCode == STATUS_SINGLE_STEP) {
+		for (int i = 0; i < _sizeOfInfo; i++) {
+			if (!_accessInfo[i].inUse) {
+				
+				int hit = ExceptionInfo->ContextRecord->Dr6 & 0xF;
+				if (hit & 8) {
+					_accessInfo[i].addressAccessed = ExceptionInfo->ContextRecord->Dr3;
+				}else if (hit & 4) {
+					_accessInfo[i].addressAccessed = ExceptionInfo->ContextRecord->Dr2;
+				}else if (hit & 2) {
+					_accessInfo[i].addressAccessed = ExceptionInfo->ContextRecord->Dr1;
+				}else if (hit & 1) {
+					_accessInfo[i].addressAccessed = ExceptionInfo->ContextRecord->Dr0;
+				}
+
+				RtlCaptureStackBackTrace(4, _framesToCapture, (void**)(_accessInfo[i].frameTrace), nullptr);
+				_accessInfo[i].inUse = true;
+			}
+		}
+		return EXCEPTION_CONTINUE_EXECUTION;
+	}
 
 	if (ExceptionInfo->ExceptionRecord->ExceptionCode == STATUS_GUARD_PAGE_VIOLATION) {
 		_addressHit = ExceptionInfo->ExceptionRecord->ExceptionInformation[1];
 		for (int i = 0; i < _sizeofAddresses; i++) {
-			if(_addresses->watched)
-			{
 				if (_addresses[i].addressToWatch == 0)
 					goto end;
-				if (_addresses[i].addressToWatch != 0 && ExceptionInfo->ExceptionRecord->ExceptionInformation[1] >= (unsigned __int64)_addresses[i].addressToWatch && ExceptionInfo->ExceptionRecord->ExceptionInformation[1] <= (unsigned __int64)_addresses[i].addressToWatch + _addresses[i].sizeOfType) {
-					_addresses[i].watched = false; //page needs to be reset
+				if (_addresses[i].addressToWatch != 0 && ExceptionInfo->ExceptionRecord->ExceptionInformation[1] >= (unsigned __int64)_addresses[i].addressToWatch && ExceptionInfo->ExceptionRecord->ExceptionInformation[1] <= (unsigned __int64)_addresses[i].addressToWatch + _addresses[i].sizeOfType) {//page needs to be reset
 					if (_addresses->operation == ReadWrite || _addresses->operation == ExceptionInfo->ExceptionRecord->ExceptionInformation[0])
 					{
 						for (int i = 0; i < _sizeOfInfo; i++) {
@@ -149,17 +181,29 @@ static long CALLBACK VectoredExceptionHandler(PEXCEPTION_POINTERS ExceptionInfo)
 					else {
 						goto end;
 					}
-				}
 			}
 		}
-		end:
-		ExceptionInfo->ContextRecord->EFlags |= 0x100;
+	end:
+		// for simplicity
+		auto* ctx = ExceptionInfo->ContextRecord;
+
+		const uint64_t oneAddressdr7 = 0x0000000000dd0405;
+		const uint64_t twoAddressdr7 = 0x00000000dddd0455;
+
+
+		ExceptionInfo->ContextRecord->Dr0 = static_cast<DWORD64>(_addresses[0].addressToWatch + 0x4);
+		ExceptionInfo->ContextRecord->Dr1 = static_cast<DWORD64>(_addresses[0].addressToWatch + 0x1E);
+		ExceptionInfo->ContextRecord->Dr7 = oneAddressdr7;
+
+		if (_sizeofAddresses > 1 && _addresses[1].addressToWatch != 0) {
+			ExceptionInfo->ContextRecord->Dr2 = static_cast<DWORD64>(_addresses[1].addressToWatch + 0x4);
+			ExceptionInfo->ContextRecord->Dr3 = static_cast<DWORD64>(_addresses[1].addressToWatch + 0x1E);
+
+			ExceptionInfo->ContextRecord->Dr7 = twoAddressdr7;
+		}
+
+
 		return EXCEPTION_CONTINUE_EXECUTION;	//Swallow all the page guards cause idgaf
-	}
-	else if (ExceptionInfo->ExceptionRecord->ExceptionCode == STATUS_SINGLE_STEP) {
-		GuardPage(_addressHit);
-		_addressHit = 0;
-		return EXCEPTION_CONTINUE_EXECUTION;
 	}
 	return EXCEPTION_CONTINUE_SEARCH;
 }
